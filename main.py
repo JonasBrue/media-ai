59e5a1f5035bd07eda7cc222140fd3c9c73d2db4
JonasBrue <jonas.brueggemann01@gmail.com>
2023-11-08 14:07:41 +0100

    Implemented automatic extraction of audio files in mp3 format from youtube videos.

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pytube import YouTube
+import os
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def mp3_from_youtube_video(url):
+    path_to_storage = "./temp/audio/"
+
+    try:
+        yt = YouTube(url)  # A YouTube object with the url of the video to be downloaded.
+        path_to_file = path_to_storage + yt.video_id + ".mp3"
+        print("Found video: '" + yt.title + "' by '" + yt.author + "'")
+    except:
+        print("Incorrect input or internet connection not possible.")  # to handle exception
+
+    try:
+        if os.path.exists(path_to_file):  # Check if the mp3 is already downloaded.
+            print("File already exists at: " + path_to_file)
+        else:
+            audio = yt.streams.filter(only_audio=True, file_extension="mp4").last()  # Find a stream that only has audio
+
+            print("Starting to download, please wait...")
+            download = audio.download(output_path=path_to_storage)
+            print("Download successful.")
+
+            os.rename(download, path_to_file)  # Turn the file into a mp3 file
+            print("Converted to mp3 at: " + path_to_file)
+        return path_to_file
+    except:
+        print("Download failed. Check your internet connection.")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mp3 = mp3_from_youtube_video("")
